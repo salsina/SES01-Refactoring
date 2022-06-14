04d90da915db7b1740b62def54f4dddb6e56fc38
sina <sina99.sn@gmail.com>
2022-06-14 15:48:55 +0430

    hasPassedCourse method added to student

--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,4 +1,6 @@
 package domain;
+import domain.exceptions.EnrollmentRulesViolationException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -67,5 +69,15 @@ public class Student {
 	
 	public String toString() {
 		return name;
+	}
+
+	public boolean hasPassedCourse(Course course){
+		for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+			for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+				if (r.getKey().equals(course) && r.getValue() >= 10)
+					return true;
+			}
+		}
+		return false;
 	}
 }
